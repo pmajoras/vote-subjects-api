bb40b3ba04aa7f3cbc69b1a338bf4b7ad510588f
Paulo Gabriel Souza Poffal <pgabrielpoffal@gmail.com>
2018-09-14 02:31:40 -0300

    VoteSubjects|Implemented Controller for subject model
    Changes:
    Implemented Subject Controller with custom method to getbyId, in order to be able to use the SLUG as id as well.

---
+++ src/main/java/com/associates/votesubjects/controllers/SubjectController.java
@@ -0,0 +1,24 @@
+package com.associates.votesubjects.controllers;
+
+import com.associates.votesubjects.core.BaseController;
+import com.associates.votesubjects.core.Utils;
+import com.associates.votesubjects.core.errors.EntityNotFoundException;
+import com.associates.votesubjects.models.Subject;
+import com.associates.votesubjects.repositories.SubjectRepository;
+import com.associates.votesubjects.services.SubjectService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController extends BaseController<Subject, SubjectService, SubjectRepository> {
+
+    @Override
+    @GetMapping("/{slugOrId}")
+    public Subject getById(@Valid @PathVariable("slugOrId") final String slugOrId) {
+        return service.findBySlugOrId(slugOrId).orElseThrow(EntityNotFoundException::new);
+    }
+}
