cabfec26f061fe4eee9d61a386f332b5aa110cf8
Paulo Gabriel Souza Poffal <pgabrielpoffal@gmail.com>
2018-09-16 23:18:16 -0300

    VoteSubjects|Added a version method in root request, just to check if API is online.

---
+++ src/main/java/com/associates/votesubjects/controllers/VersionController.java
@@ -0,0 +1,15 @@
+package com.associates.votesubjects.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class VersionController {
+
+    @GetMapping
+    public String getVersion() {
+        return "v1.0.0";
+    }
+}
